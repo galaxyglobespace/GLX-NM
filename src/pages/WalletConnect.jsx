337fd4083d49c56b1f1f4ba99b42f02ecd6e40b5
galaxyglobespace <102635196+galaxyglobespace@users.noreply.github.com>
2022-05-13 17:23:11 +0500

    Update WalletConnect.jsx

--- src/pages/WalletConnect.jsx
+++ src/pages/WalletConnect.jsx
@@ -2,15 +2,14 @@ import React , { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Header from '../components/header/Header';
 import Footer from '../components/footer/Footer';
-
 import img1 from '../assets/images/icon/connect-1.png'
-import img2 from '../assets/images/icon/connect-2.png'
-import img3 from '../assets/images/icon/connect-3.png'
+// import img2 from '../assets/images/icon/connect-2.png'
+// import img3 from '../assets/images/icon/connect-3.png'
 import img4 from '../assets/images/icon/connect-4.png'
 import img5 from '../assets/images/icon/connect-5.png'
-import img6 from '../assets/images/icon/connect-6.png'
-import img7 from '../assets/images/icon/connect-7.png'
-import img8 from '../assets/images/icon/connect-8.png'
+// import img6 from '../assets/images/icon/connect-6.png'
+// import img7 from '../assets/images/icon/connect-7.png'
+// import img8 from '../assets/images/icon/connect-8.png'
 
 const WalletConnect = () => {
     const [data] = useState(
@@ -62,16 +61,16 @@ const WalletConnect = () => {
             <section className="flat-title-page inner">
                 <div className="overlay"></div>
                 <div className="themesflat-container">
-                    <div className="row">
-                        <div className="col-md-12">
+                    <div className="col">
+                        <div className="row-md-12">
                             <div className="page-title-heading mg-bt-12">
-                                <h1 className="heading text-center">Connect</h1>
+                                <h1 className="heading text-center">Connect Wallet</h1>
                             </div>
                             <div className="breadcrumbs style2">
                                 <ul>
                                     <li><Link to="/">Home</Link></li>
                                     <li><Link to="#">Pages</Link></li>
-                                    <li>Connect</li>
+                                    <li>Connect Wallet</li>
                                 </ul>
                             </div>
                         </div>
@@ -80,7 +79,7 @@ const WalletConnect = () => {
             </section>
             <div className="tf-connect-wallet tf-section">
                 <div className="themesflat-container">
-                    <div className="row">
+                    <div className="col">
                         <div className="col-12">
                             <h2 className="tf-title-heading ct style-2 mg-bt-12">
                                 Connect Your Wallet
@@ -89,15 +88,16 @@ const WalletConnect = () => {
                             </h5>
                         </div>
                         <div className="col-md-12">
-                            <div className="sc-box-icon-inner style-2">
+                            <div className="sc-box-icon-inner style-2 fl-button pri-1">
                                 {
                                     data.map((item,index) => (
                                         <div key={index} className="sc-box-icon">
                                             <div className="img">
-                                                <img src={item.img} alt="Axies" />
+                                                <img src={item.img} alt="GLX" />
                                             </div>
                                             <h4 className="heading"><Link to="/login">{item.title}</Link> </h4>
                                             <p className="content">{item.description}</p>
+                                            
                                          </div>
                                     ))
                                 }
