a7991b099a3b6d39df8d3bc81b5b3b4e131e6a51
itssaptarshi <saptarshimaitra91@gmail.com>
2022-05-13 19:35:14 +0530

    Moralis Integration

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom'
 import ScrollToTop from './ScrollToTop';
+import { MoralisProvider } from "react-moralis";
+
 
 ReactDOM.render(
   <BrowserRouter >
     <ScrollToTop />
+    <MoralisProvider serverUrl="https://btbpgajnttxt.usemoralis.com:2053/server" appId="cg083RyJzWvVbLtyQ7WxQpfAyIICON4AN8MGSxqe">
     <App />
+    </MoralisProvider>
   </BrowserRouter>,
   document.getElementById('root')
 );
