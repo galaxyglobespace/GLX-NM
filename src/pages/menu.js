a1189416a08bee9d1d39b99e5680ea46055e9a69
Galaxy Space <galaxyglobespace@gmail.com>
2022-05-13 13:23:01 +0500

    initial commit

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,106 +1,111 @@
 const menus = [
     {
         id: 1,
-        name: 'Home',
-        links: '#',
-        namesub: [
-            {
-                id: 1,
-                sub: 'Home 01',
-                links: '/'
-            },
-            {
-                id: 2,
-                sub: 'Home 02',
-                links: '/home-02'
-            },
-            {
-                id: 3,
-                sub: 'Home 03',
-                links: '/home-03'
-            },
-            {
-                id: 4,
-                sub: 'Home 04',
-                links: '/home-04'
-            },
-            {
-                id: 5,
-                sub: 'Home 05',
-                links: '/home-05'
-            },
-            {
-                id: 6,
-                sub: 'Home 06',
-                links: '/home-06'
-            },
-            {
-                id: 7,
-                sub: 'Home 07',
-                links: '/home-07'
-            },
-            {
-                id: 8,
-                sub: 'Home 08 (Special)',
-                links: '/home-08'
-            },
-        ]
+        name: '',
+        links: '/home-08',
+        // namesub: [
+        //     {
+        //         id: 1,
+        //         sub: 'Home 01',
+        //         links: '/'
+        //     },
+        //     {
+        //         id: 2,
+        //         sub: 'Home 02',
+        //         links: '/home-02'
+        //     },
+        //     {
+        //         id: 3,
+        //         sub: 'Home 03',
+        //         links: '/home-03'
+        //     },
+        //     {
+        //         id: 4,
+        //         sub: 'Home 04',
+        //         links: '/home-04'
+        //     },
+        //     {
+        //         id: 5,
+        //         sub: 'Home 05',
+        //         links: '/home-05'
+        //     },
+        //     {
+        //         id: 6,
+        //         sub: 'Home 06',
+        //         links: '/home-06'
+        //     },
+        //     {
+        //         id: 7,
+        //         sub: 'Home 07',
+        //         links: '/home-07'
+        //     },
+        //     {
+        //         id: 8,
+        //         sub: 'Home 08 (Special)',
+        //         links: '/home-08'
+        //     },
+        // ]
     },
     {
         id: 2,
         name: 'Explore',
-        links: '#',
+        links: '/explore-04',
         namesub: [
-            {
-                id: 1,
-                sub: 'Explore 01',
-                links: '/explore-01'
-            },
-            {
-                id: 2,
-                sub: 'Explore 02',
-                links: '/explore-02'
-            },
-            {
-                id: 3,
-                sub: 'Explore 03',
-                links: '/explore-03'
-            },
-            {
-                id: 4,
-                sub: 'Explore 04',
-                links: '/explore-04'
-            },
+            // {
+            //     id: 1,
+            //     sub: 'Explore 01',
+            //     links: '/explore-01'
+            // },
+            // {
+            //     id: 2,
+            //     sub: 'Explore 02',
+            //     links: '/explore-02'
+            // },
+            // {
+            //     id: 3,
+            //     sub: 'Explore 03',
+            //     links: '/explore-03'
+            // },
+            // {
+            //     id: 4,
+            //     sub: 'Explore 04',
+            //     links: '/explore-04'
+            // },
             {
                 id: 5,
                 sub: 'Live Auctions',
                 links: '/live-auctions'
             },
-            {
-                id: 6,
-                sub: 'Item Details 01',
-                links: '/item-details-01'
-            },
+            // {
+            //     id: 6,
+            //     sub: 'Item Details 01',
+            //     links: '/item-details-01'
+            // },
             {
                 id: 7,
-                sub: 'Item Details 02',
+                sub: 'Item Details',
                 links: '/item-details-02'
             }
             
         ],
     },{
         id: 3,
-        name: 'Activity',
+        name: 'Stats',
         links: '#',
         namesub: [
+            // {
+            //     id: 1,
+            //     sub: 'Activity 01',
+            //     links: '/activity-01'
+            // },
             {
-                id: 1,
-                sub: 'Activity 01',
-                links: '/activity-01'
+                id: 6,
+                sub: 'Ranking',
+                links: '/ranking'
             },
             {
                 id: 2,
-                sub: 'Activity 02',
+                sub: 'Activity',
                 links: '/activity-02'
             }
         ],
@@ -121,6 +126,16 @@ const menus = [
                 links: '/blog-details'
             },
             {
+                id: 1,
+                sub: 'Contact',
+                links: '/contact-01'
+            },
+            {
+                id: 10,
+                sub: 'FAQ',
+                links: '/faq'
+            },
+            {
                 id: 3,
                 sub: 'Help Center',
                 links: '/help-center'
@@ -129,77 +144,65 @@ const menus = [
     },
     {
         id: 5,
-        name: 'Page',
+        name: 'Profile',
         links: '#',
         namesub: [
-            {
-                id: 1,
-                sub: 'Authors 01',
-                links: '/authors-01'
-            },
+            // {
+            //     id: 1,
+            //     sub: 'Authors 01',
+            //     links: '/authors-01'
+            // },
             {
                 id: 2,
-                sub: 'Authors 02',
+                sub: 'Authors',
                 links: '/authors-02'
             },
-            {
-                id: 3,
-                sub: 'Wallet Connect',
-                links: '/wallet-connect'
-            },
-            {
-                id: 4,
-                sub: 'Create Item',
-                links: '/create-item'
-            },
+            // {
+            //     id: 3,
+            //     sub: 'Wallet Connect',
+            //     links: '/wallet-connect'
+            // },
+            // {
+            //     id: 4,
+            //     sub: '',
+            //     links: '/create-item'
+            // },
             {
                 id: 5,
                 sub: 'Edit Profile',
                 links: '/edit-profile'
             },
-            {
-                id: 6,
-                sub: 'Ranking',
-                links: '/ranking'
-            },
-            {
-                id: 7,
-                sub: 'Login',
-                links: '/login'
-            },
-            {
-                id: 8,
-                sub: 'Sign Up',
-                links: '/sign-up'
-            },
+            
+            // {
+            //     id: 7,
+            //     sub: 'Login',
+            //     links: '/login'
+            // },
+            // {
+            //     id: 8,
+            //     sub: 'Sign Up',
+            //     links: '/sign-up'
+            // },
             {
                 id: 9,
                 sub: 'No Result',
                 links: '/no-result'
             },
-            {
-                id: 10,
-                sub: 'FAQ',
-                links: '/faq'
-            }
+            
         ],
     },
     {
         id: 7,
-        name: 'Contact',
-        links: '/contact-01',
-        namesub: [
-            {
-                id: 1,
-                sub: 'Contact 1',
-                links: '/contact-01'
-            },
-            {
-                id: 2,
-                sub: 'Contact 2',
-                links: '/contact-02'
-            }
-        ],
+        name: 'Create',
+        links: '/create-item',
+        // namesub: [
+
+        //     // {
+        //     //     id: 2,
+        //     //     sub: 'Contact 2',
+        //     //     links: '/contact-02'
+        //     // }
+        // ],
     },
     
 ]
